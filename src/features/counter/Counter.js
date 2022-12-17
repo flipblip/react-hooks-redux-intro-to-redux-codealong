3062000bbde902d3924269eef20fd5ece514ed35
thrasher <thrasher@viper.moringa>
2022-12-17 11:32:27 -0600

    Done

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 
 function Counter() {
   // read from the Redux store
+  // we're calling useSelector with a callback function, returning
+  // the items key from our Redux store state.
   const items = useSelector((state) => state.items);
 
   // gives us the dispatch function to send actions to the Redux store
--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { legacy_createStore as createStore } from "redux";
 import counterReducer from "./features/counter/counterSlice.js";
 import App from "./App";
 import "./index.css";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(counterReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>,
+document.getElementById("root"));
